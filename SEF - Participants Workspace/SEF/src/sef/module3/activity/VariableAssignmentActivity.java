9782be62fc97f299584f61f446122d51d7a9cbc7
Aizhan Mambetalieva <aijan312kg@gmail.com>
2021-03-02 21:10:44 +0200

    Activity: variable assignment

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -17,6 +17,9 @@ public class VariableAssignmentActivity {
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
+		int a = 5;
+		a = 12;
+		System.out.println("number = " + a);
 
 	}
 
