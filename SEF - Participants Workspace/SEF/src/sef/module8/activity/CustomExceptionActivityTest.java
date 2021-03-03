fccb344144f30d0a4110250757e545764b72b024
Aizhan Mambetalieva <aijan312kg@gmail.com>
2021-03-03 20:19:27 +0200

    Activity: custom exception

--- SEF - Participants Workspace/SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
+++ SEF - Participants Workspace/SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
@@ -6,26 +6,37 @@ package sef.module8.activity;
 public class CustomExceptionActivityTest {
 
 	//1 - Type main method and call validateUser() from it.
-	//Call this method with different names to test it 
+	//Call this method with different names to test it
+    public static void main(String[] args) {
+        CustomExceptionActivityTest c = new CustomExceptionActivityTest();
+        try {
+            c.validateUser("Mike");
+            c.validateUser("Aizhan");
+        } catch (CustomExceptionActivity ex) {
+            System.out.println("Exception message: " + ex.getMessage());
+        }
+    }
 
+    public void validateUser(String name) throws CustomExceptionActivity {
+        //array of names
+        String[] validUsers = { "John", "Mike", "Shanti", "Stacie" };
+        //flag stores 1 if a match is found else it should remain 0
+        int flag = 0;
+        for (int i = 0; i < 4; i++) {
+            //2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
+            if (name.equals(validUsers[i])) {
+                flag = 1;
+            }
+        }
+        //3 - check if flag is zero, throw CustomExceptionActivity Exception.
+        //You may also have to declare this exception in the method call using throws.
+        if (flag == 0) {
+            throw new CustomExceptionActivity();
+        } else if (flag == 1) {
+            System.out.println("Welcome to Payroll program");
+        }
 
-void validateUser(String name)
-{
-	//array of names
-String []validUsers={"John","Mike","Shanti","Stacie"};
-//flag stores 1 if a match is found else it should remain 0 
-int flag=0;
-for(int i=0;i<4;i++)
-{
-	
-//2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly 
+        //4 - else if flag is one, print a message "Welcome to Payroll program".
 
-}
-//3 - check if flag is zero, throw CustomExceptionActivity Exception.
-//You may also have to declare this exception in the method call using throws.
-
-
-//4 - else if flag is one, print a message "Welcome to Payroll program". 
-
-}
+    }
 }
